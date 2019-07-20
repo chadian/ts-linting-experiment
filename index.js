73760d3ea30f802f96cb3109d287e268a5a3b509
Chad Carbert <chadcarbert@me.com>
2019-07-20 16:00:44 -0400

    Remove .js files

--- index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var useSparingly_1 = require("./useSparingly");
-console.log(useSparingly_1["default"]());
-console.log(useSparingly_1["default"]());
-console.log(useSparingly_1["default"]());
