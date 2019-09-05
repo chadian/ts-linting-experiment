9eb5eaa564771155cbc2bdfa5b6383aef1d75524
Chad Carbert <chadcarbert@me.com>
2019-09-05 19:52:43 -0400

    Clean up example further

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,5 @@ console.log(dontUseMeTooOften());
 console.log(dontUseMeTooOften());
 console.log(dontUseMeTooOften());
 
-// the current rule is configured for only four calls,
-// the fifth call of this function will trigger a tslint error
+// Four times is okay BUT... Five? No way!
 console.log(dontUseMeTooOften());
