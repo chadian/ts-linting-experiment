92ade8591e6bc441e26c4eb8dad341d19825bbf4
Chad Carbert <chadcarbert@me.com>
2019-09-05 19:44:49 -0400

    Add check that file to track exists

--- rules/useSparinglyRule.ts
+++ rules/useSparinglyRule.ts
@@ -1,5 +1,6 @@
 import maxUsageRule from './meta-rules/max-usage';
 import { resolve } from 'path';
+import { existsSync as fileExists } from 'fs';
 
 export class Rule extends maxUsageRule({
   ruleName: 'use-sparingly',
@@ -7,6 +8,10 @@ export class Rule extends maxUsageRule({
   matchesImportSignature: (path: string, exportedName: string) => {
     let trackedPath = resolve(__dirname, '../src', 'someCrazyImplementation');
     let trackedExportName = 'default';
+
+    if (!fileExists(`${trackedPath}.ts`)) {
+    	throw `{this.ruleName} can't track usage against non-existing file: ${trackedPath}`;
+    }
 
     // path is including double-quotes from symbol so
     // we wrap out trackedPath with double-quotes for comparison
